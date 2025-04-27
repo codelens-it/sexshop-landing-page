84ff178a553d26f15275c809846cb8251c4fd749
Loreaylen <lag9512@gmail.com>
2025-04-27 00:05:14 -0300

    Feat: Navbar
    Css de menú expandido. Se agrega botón para cerrar menú

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,19 +1,35 @@
 import './navbar.css'
+import { useState, useEffect } from 'react'
 
 const Navbar = () => {
+  const tabletMedia = '768'
+  const [isTablet, setIsTablet] = useState(false)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsTablet(window.innerWidth <= tabletMedia)
+      return   
+    }
+    window.addEventListener('resize', handleResize)
+
+    return () => window.removeEventListener('resize', handleResize)
+  })
 
   return (
     <nav id='navbar'>
       <div className='logo-container'>
-      <img  className='nav-logo' src="/public/logo/logo.svg" alt="Logo" />
+        <img className='nav-logo' src="/public/logo/logo.svg" alt="Logo" />
+      </div>
+      <button><img src='/icons/burger-menu.svg' alt='icono de menú' className='burger-menu' /></button>
+      <div className="menu-list-container">
+        <ul className="menu-list">
+          <li><a href="#">Inicio</a></li>
+          <li><a href="#">Sobre nosotros</a></li>
+          <li><a href="#">Catálogo</a></li>
+          <li><a href="#">Contacto</a></li>
+        </ul>
       </div>
 
-    <ul className="nav-menu">
-      <li><a href="#">Inicio</a></li>
-      <li><a href="#">Sobre nosotros</a></li>
-      <li><a href="#">Catálogo</a></li>
-      <li><a href="#">Contacto</a></li>
-    </ul>
     </nav>
   )
 }
