99a1dd657d2c057008eeb9fc61d6f3791d097aa6
Loreaylen <lag9512@gmail.com>
2025-04-27 13:08:01 -0300

    Feat: Navbar
    Creo hook para comprobar el ancho de la ventana. Cambio ruta de logo para sacar el public.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,24 +1,15 @@
 import './navbar.css'
-import { useState, useEffect } from 'react'
+import { useWindowSize } from '../../hooks/useWindowSize';
 
 const Navbar = () => {
-  const tabletMedia = '768'
-  const [isTablet, setIsTablet] = useState(false)
 
-  useEffect(() => {
-    const handleResize = () => {
-      setIsTablet(window.innerWidth <= tabletMedia)
-      return   
-    }
-    window.addEventListener('resize', handleResize)
-
-    return () => window.removeEventListener('resize', handleResize)
-  })
+  const sizes = useWindowSize()
+  console.log(sizes)
 
   return (
     <nav id='navbar'>
-      <div className='logo-container open-menu'>
-        <img className='nav-logo' src="/public/logo/logo.svg" alt="Logo" />
+      <div className='logo-container'>
+        <img className='nav-logo' src="/logo/logo.svg" alt="Logo" />
       </div>
       <button className='menu-button'><img src='/icons/burger-menu.svg' alt='icono de menú' className='burger-menu' /></button>
       <div className="menu-list-container">
