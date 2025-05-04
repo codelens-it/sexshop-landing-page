fa1e5ff2b2b861465949c281ede08f6e085d7f3a
Yanina-Unrein <unrein.syanina@gmail.com>
2025-05-04 18:19:00 -0300

    mejoras responsividad slider

--- src/data/sliderImages.js
+++ src/data/sliderImages.js
@@ -2,6 +2,8 @@ const sliderImages = [
     {
       id: 1,
       url: "/slider/slider1.svg",
+      mobileUrl: "/slider/slider1.svg",
+      desktopUrl: "/slider/slider1.svg",
       alt: "Descripción de la primera imagen",
       title: "Título imagen 1",
       description: "Texto descriptivo opcional"
@@ -9,6 +11,8 @@ const sliderImages = [
     {
       id: 2,
       url: "/slider/cuotas.svg",
+      mobileUrl: "/slider/mobile_cuotas.svg",
+      desktopUrl: "/slider/cuotas.svg",
       alt: "Descripción de la segunda imagen",
       title: "Título imagen 2",
       description: "Texto descriptivo opcional"
@@ -16,6 +20,8 @@ const sliderImages = [
     {
       id: 3,
       url: "/slider/bdsm.svg",
+      mobileUrl: "/slider/bdsm.svg",
+      desktopUrl: "/slider/bdsm.svg",
       alt: "Descripción de la tercera imagen",
       title: "Título imagen 3",
       description: "Texto descriptivo opcional"
@@ -23,6 +29,8 @@ const sliderImages = [
     {
         id: 4,
         url: "/slider/bds.svg",
+        mobileUrl: "/slider/bds.svg",
+        desktopUrl: "/slider/bds.svg",
         alt: "Descripción de la tercera imagen",
         title: "Título imagen 4",
         description: "Texto descriptivo opcional"
