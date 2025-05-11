bae5c7f389dc9c982fc2fec77db23c39822896dc
Loreaylen <lag9512@gmail.com>
2025-05-11 20:13:34 -0300

    Fix: Catálogo
    Se implementa el Loader como fallback en caso de que no se hayan cargado las cards

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -3,6 +3,7 @@ import { useContext } from "react";
 import { CatalogContext } from '@/context/CatalogContext'
 import { GalleryCard } from "@/components/GalleryCard/GalleryCard";
 import { Categories } from '../categories/Categories';
+import Loader from '../loader/Loader';
 
 const Gallery = () => {
   const { cardList } = useContext(CatalogContext)
@@ -12,13 +13,18 @@ const Gallery = () => {
   return (<div className="section gallery">
     <h2>Tu próxima aventura comienza acá</h2>
     <span>Explorá una selección íntima pensada para despertar tus sentidos. Productos únicos, atención discreta y placer a tu medida.</span>
-    <Categories/>
-    <div className="catalog-container">
-      {
-        cards.map((card) => (<GalleryCard key={card.id} productName={card.name} description={card.description} img={card.image} alt={card.imgAlt} />
-        ))
-      }
-    </div>
+    <Categories />
+
+    {!cards ?
+      <Loader/>
+      :
+      <div className="catalog-container">
+        {
+          cards.map((card) => (<GalleryCard key={card.id} productName={card.name} description={card.description} img={card.image} alt={card.imgAlt} />
+          ))
+        }
+      </div>}
+
   </div>)
 }
 
