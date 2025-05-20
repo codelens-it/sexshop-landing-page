777abd64b5e05e1ba14de5d1f4292da81d8fd0e5
Loreaylen <lag9512@gmail.com>
2025-05-20 08:32:53 -0300

    Feat: Modal detalle de producto
    Se completa estructura de modal

--- src/components/productModal/ProductModal.jsx
+++ src/components/productModal/ProductModal.jsx
@@ -2,6 +2,7 @@ import './productModal.css'
 import { useContext, useEffect, useRef } from 'react'
 import { CatalogContext } from '@/context/CatalogContext.jsx'
 import ImageCarousel from '@/components/imageCarousel/ImageCarousel.jsx'
+import AccordionItem from '@/components/accordionItem/AccordionItem.jsx'
 
 
 const ProductModal = () => {
@@ -26,11 +27,50 @@ const ProductModal = () => {
   return (
     <dialog id='product-modal-container' ref={modalRef}>
       <div id='product-detail'>
-        <h3>hola</h3>
+        <h3>{product.name}</h3>
         <ImageCarousel images={product.images}/>
-        <p>{product.name}</p>
-        <button onClick={() => onClose()}><img src="/icons/close-menu.svg" alt="Cerrar modal" /></button>
+        <p>{product.description}</p>
+        <AccordionItem title={"Características"}>
+        <ul>
+          {product.features.map((feature, i) => {
+            return <li key={i}>{feature}</li>
+          })}
+        </ul>
+        </AccordionItem>
+        <AccordionItem title={"Medidas"}>
+          {product.measurements.type === 'dimensions' ? 
+            (<ul>
+              <li>Ancho: {product.measurements.dimensions.width}</li>
+              <li>Alto: {product.measurements.dimensions.height}</li>
+              <li>Grosor: {product.measurements.dimensions.thickness}</li>
+            </ul>)
+            : (
+              <div>
+                <span>Talles disponibles:</span>
+                <ul>
+                  {product.measurements.sizes?.map((size) => {
+                    return <li>{size}</li>
+                  })}
+                </ul>
+              </div>
+            )  
+        }
+        </AccordionItem>
+        <AccordionItem title={"Precio y disponibilidad"}>
+          <div>
+            <span>{new Intl.NumberFormat('es-AR', {
+              style: "currency",
+              currency: "ARS"
+            }).format(product.price)
+            }</span>
+            <br/>
+            <span>{product.inStock ? "Disponible" : "Sin Stock"}</span>
+          </div>
+        </AccordionItem>
       </div>
+      <button onClick={() => onClose()}>
+        <img src="/icons/close-menu.svg" alt="Cerrar modal" />
+      </button>
     </dialog>
   )
 }
