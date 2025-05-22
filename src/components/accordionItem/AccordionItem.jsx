8e6d0cd942448bf8f4f702305c1ac294e1a4072d
Loreaylen <lag9512@gmail.com>
2025-05-22 00:39:41 -0300

    Feat: Modal detalle de producto
    Css de modal. Se agrega css de acordeones

--- src/components/accordionItem/AccordionItem.jsx
+++ src/components/accordionItem/AccordionItem.jsx
@@ -5,14 +5,12 @@ const AccordionItem = ({title, children}) => {
   const [isOpen, setIsOpen] = useState(false)
 
   return(
-    <div className='accordion-item-container' onClick={() => setIsOpen(!isOpen)}>
-      <div className='accordion-item-title'>
+    <div className={`accordion-item-container ${isOpen && 'open'}`} onClick={() => setIsOpen(!isOpen)}>
+      <button className='accordion-item-title'>
         <span>{title}</span>
-        <button className='accordion-item-expand-btn'>
-          <img src="/icons/expand-arrow.svg" alt="Expand button" />
-          </button>
-      </div>
-      <div lassName='accordion-item-content'>
+          <img className='accordion-arrow' src="/icons/expand-arrow.svg" alt="Expand button" />
+      </button>
+      <div className='accordion-item-content'>
         {children}
       </div>
     </div>
